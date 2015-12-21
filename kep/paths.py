0be20cc0eb3251b37c139ca2a932f07d77ae2a1c
Alexander Lukanin <alexander.lukanin.13@gmail.com>
2015-12-21 21:34:46 +0500

    Changed kep.paths to use absolute paths

--- kep/paths.py
+++ kep/paths.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
 
-# data source 
+# data source
+PACKAGE_FOLDER = os.path.join(os.path.dirname(__file__))
+DATA_FOLDER = os.path.normpath(os.path.join(PACKAGE_FOLDER, '..', 'data'))
 CURRENT_MONTH_DATA_FOLDER = os.path.join('data', '2015', 'ind10')
 
 # database file
-DB_FILE = os.path.join('kep', 'database', 'kep.sqlite')
+DB_FILE = os.path.join(PACKAGE_FOLDER, 'database', 'kep.sqlite')
 
 # output
-OUTPUT_DIR = 'output'
+OUTPUT_DIR = os.path.normpath(os.path.join(PACKAGE_FOLDER, '..', 'output'))
 PDF_FILE      = os.path.join(OUTPUT_DIR, 'monthly.pdf')
 MD_PATH       = os.path.join(OUTPUT_DIR, 'images.md')
 PNG_FOLDER    = os.path.join(OUTPUT_DIR, 'png')
@@ -22,7 +24,7 @@ MONTHLY_CSV   = os.path.join(OUTPUT_DIR, 'data_monthly.txt')
 VARNAMES_FILE = os.path.join(OUTPUT_DIR, 'varnames.md')
 
 # temp folder for testing
-SUBFOLDER = os.path.join('kep', 'test', 'temp')
+SUBFOLDER = os.path.join(PACKAGE_FOLDER, 'test', 'temp')
 
 # inspection files (not used now)
 INSPECTION_FOLDER = os.path.join('kep', 'inspection')
